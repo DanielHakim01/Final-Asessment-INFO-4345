27f49adc06eb451969aab99fa3506bdbf4ae924f
DanielHakim01 <107184092+DanielHakim01@users.noreply.github.com>
2023-07-07 14:42:24 +0800

    Update menuGP.php

--- After/Views/menuGP.php
+++ After/Views/menuGP.php
@@ -1,18 +1,16 @@
 <?php
-require_once('../html/idle.php');  
+require_once('../html/idle.php');
 
 
 // Check if the CSRF token exists in the session
 if (isset($_SESSION['csrf_token'])) {
-    $csrf_token = $_SESSION['csrf_token'];
+  $csrf_token = $_SESSION['csrf_token'];
 } else {
-    // CSRF token is not found, redirect to login page
-    echo '<script>alert("Error: CSRF token not found."); window.location.href = "login.php";</script>';
-    exit();
+  // CSRF token is not found, redirect to login page
+  echo '<script>alert("Error: CSRF token not found."); window.location.href = "login.php";</script>';
+  exit();
 }
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,88 +18,13 @@ if (isset($_SESSION['csrf_token'])) {
 <head>
   <title>MENU</title>
   <link rel="stylesheet" href="../CSS_JS/faciinfo.css">
-  <style>
-    body {
-      background-color: #f1f1f1;
-      color: #1b9284;
-      font-family: Arial, sans-serif;
-    }
-
-    .button {
-      background-color: #1b9284;
-      border: none;
-      color: rgb(255, 255, 255);
-      padding: 15px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-      border-radius: 25px;
-    }
-
-    .main {
-      text-align: center;
-      margin-top: 30px;
-    }
-
-    .pill-nav {
-      background-color: #f1f1f1;
-      overflow: hidden;
-    }
-
-    .pill-nav a {
-      color: #1b9284;
-      float: left;
-      text-align: center;
-      padding: 14px 16px;
-      text-decoration: none;
-      font-size: 17px;
-    }
-
-    .pill-nav a.active {
-      background-color: #1b9284;
-      color: white;
-    }
-
-    .pill-nav a:hover {
-      background-color: #ddd;
-      color: #1b9284;
-    }
-
-    h1 {
-      color: #1b9284;
-    }
-
-    .row {
-      display: flex;
-      flex-wrap: wrap;
-      padding: 0 4px;
-    }
-
-    .column {
-      flex: 25%;
-      max-width: 25%;
-      padding: 0 4px;
-    }
-
-    .column .content {
-      background-color: white;
-      margin-bottom: 15px;
-      padding: 15px;
-      text-align: center;
-    }
-
-    .content h3 {
-      color: #1b9284;
-      text-align: center;
-    }
-  </style>
+  <link rel="stylesheet" href="../CSS_JS/menuStyle.css">
 </head>
 
 <body>
   <div class="main">
     <div class="pill-nav">
-      <a style="color: #1b9284;" href="Main_Page.php">Home</a>
+      <a id="homeNavTitle" href="home.php">Home</a>;
       <a style="color: white;" href="menuGP.php" class="active">Menu</a>
       <a style="color: #1b9284;" href="faci_info.php">Facilities</a>
       <a style="color: #1b9284;" href="logout.php">Logout</a>
@@ -155,12 +78,12 @@ if (isset($_SESSION['csrf_token'])) {
 
   </div>
 
-  </body>
+</body>
 
 </html>
 
 <script>
- function redirect(sport) {
-  window.location.href = "confirm.php?sport=" + encodeURIComponent(sport);
-}
+  function redirect(sport) {
+    window.location.href = "confirm.php?sport=" + encodeURIComponent(sport);
+  }
 </script>
